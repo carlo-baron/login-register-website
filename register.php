6519b2027ce817653bc0ef10cf7e4be05cd1abb4
carlo-baron <carlobaron430@gmail.com>
2024-11-09 08:39:22 +0800

    Login and Register forms Added

--- register.php
+++ register.php
@@ -7,16 +7,16 @@
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <h1>LOGIN</h1
+    <h1>REGISTER</h1
     <form action="index.php" method="post">
         <label for="username">Username:</label><br>
         <input type="text" name="username"><br>
         <label for="username">Password:</label><br>
         <input type="password" name="password"><br>
-        <input type="submit" name="login" value="Log In">
-    </form>
-    <form action="register.php">
         <input type="submit" name="register" value="Register">
+    </form>
+    <form action="index.php">
+        <input type="submit" name="login" value="Log In">
     </form>
 </body>
 </html>
